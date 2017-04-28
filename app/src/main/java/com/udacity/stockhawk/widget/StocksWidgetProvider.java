4d8b838e4f60d094b2a6feea441252651bcf43ca
CesarPim <cesarpim@gmail.com>
2017-04-28 01:01:43 +0100

    Implemented basic widget

---
+++ app/src/main/java/com/udacity/stockhawk/widget/StocksWidgetProvider.java
@@ -0,0 +1,31 @@
+package com.udacity.stockhawk.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.ui.MainActivity;
+
+/**
+ * Created by CesarPim on 28-04-2017.
+ */
+
+public class StocksWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setTextViewText(R.id.text_widget_test, "EXPERIENCIA FABULOSA");
+
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.text_widget_test, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
